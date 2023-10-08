119b6ab262bc6307d0dcfe389f46fdc64a2530d3
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-08 06:17:39 +0530

    Add files via upload

---
+++ testbench.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+module testbench;
+
+    reg clk,rst;
+    wire [3:0] ctrl ;
+    wire [7:0] segment;
+
+    counter dut(clk, rst, ctrl, segment);
+
+    always #1 clk = ~clk;
+
+    initial begin
+        $dumpfile("testbench.vcd");
+        $dumpvars(0,testbench);
+        clk <= 1'b0;
+        $monitor("ctrl = %d, segment = %d\n",ctrl,segment);
+    end
+
+    initial #100000 $finish; //100k clock pulses
+
+endmodule
